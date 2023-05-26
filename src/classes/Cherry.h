30371cca5b19b34d8e8e9d794092bf9b54e13599
gustaleitcor <gustavo.leitecordeiro@gmail.com>
2023-05-26 16:50:00 -0300

    cherry att

--- src/classes/Cherry.h
+++ src/classes/Cherry.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "Vetor.h"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+
+using namespace std;
+using namespace cv;
 
 class Cherry
 {
 public:
     Vetor pos;
+    Mat img = cv::imread("../sprites/cherry.png", IMREAD_UNCHANGED);
 
+    Mat getImg() { return img; }
     void setPos(double x, double y);
     void getNewPos(int bordaX, int bordaY);
-}
+};
