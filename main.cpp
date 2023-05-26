3bccf7e29a4169a8055f48d459cec727b8915b25
gustaleitcor <gustavo.leitecordeiro@gmail.com>
2023-05-26 15:03:11 -0300

    pushing

--- main.cpp
+++ main.cpp
@@ -50,14 +50,12 @@ int main(int argc, const char **argv)
         return -1;
     }
 
-    // YEGO
-
-    if (!capture.open(0)) // para testar com um video
-        if (!capture.open(0))
-        {
-            cout << "Capture from camera #0 didn't work" << endl;
-            return 1;
-        }
+    if (!capture.open("rtsp://192.168.0.7:8080/h264_pcm.sdp")) // para testar com um video
+    // if(!capture.open(0))
+    {
+        cout << "Capture from camera #0 didn't work" << endl;
+        return 1;
+    }
 
     if (capture.isOpened())
     {
